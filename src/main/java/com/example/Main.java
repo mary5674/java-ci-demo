c37988145d7785486380c8c3a8337be8a7e0bd2d
mary5674 <94326118+mary5674@users.noreply.github.com>
2025-06-24 02:00:37 +0000

    Agrego una linea para prueba de despliegue

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         try (PrintWriter w = new PrintWriter("output/index.html", "UTF-8")) {
             w.println("<h1>Hello from Java CI/CD!</h1>");
+            w.println("<h2>Esto es un despliegue continuo</h2>");
             System.out.println("HTML generado correctamente.");
         } catch (IOException e) {
             e.printStackTrace();
